838333df6aa2b781c35d3d44a0cae3e779925a20
Hong <bayyyy@naver.com>
2023-09-26 14:50:39 +0900

    Test: 테스트용 로컬 데이터

--- lib/common/dio/dio_client.dart
+++ lib/common/dio/dio_client.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:retrofit/http.dart';
@@ -25,6 +27,9 @@ class FakeDioClient implements DioClient {
     required PaginationQueries paginationQueries,
     Object? requestBody,
   }) async {
+
+    await Future.delayed(Duration(seconds: 1) );
+    log('PAGINATION');
     return OffsetPagination<JobModel>(
       hasNext: false,
       content: [
@@ -42,6 +47,38 @@ class FakeDioClient implements DioClient {
           userStudentNumber: 22,
           title: '팀원 구합니다',
           date: DateTime(2023, 1, 10),
+          thumbnailUrl: 'https://picsum.photos/200/200',
+        ),
+        JobModel(
+          id: 3,
+          userMajor: '통계학부',
+          userStudentNumber: 19,
+          title: '종프 팀원 구합니다',
+          date: DateTime(2021, 10, 10),
+          thumbnailUrl: 'https://picsum.photos/200/100',
+        ),
+        JobModel(
+          id: 4,
+          userMajor: '경제학과',
+          userStudentNumber: 22,
+          title: 'ㅇㅇㅇ 구합니다',
+          date: DateTime(2023, 1, 10),
+          thumbnailUrl: 'https://picsum.photos/300/300',
+        ),
+        JobModel(
+          id: 5,
+          userMajor: '컴퓨터학부',
+          userStudentNumber: 19,
+          title: 'ㅁㄹㄹㄹ 팀원 구합니다',
+          date: DateTime(2021, 10, 10),
+          thumbnailUrl: 'https://picsum.photos/200/300',
+        ),
+        JobModel(
+          id: 6,
+          userMajor: '경제학과',
+          userStudentNumber: 22,
+          title: 'ㅇㅇㅇㅇ팀원 구합니다',
+          date: DateTime(2023, 1, 10),
           thumbnailUrl: 'https://picsum.photos/200/300',
         ),
 
@@ -53,6 +90,8 @@ class FakeDioClient implements DioClient {
   Future<JobDetailModel> getDetailJobModel({
     required int id,
   })async {
+    await Future.delayed(Duration(seconds: 1) );
+    log('getDetailJobModel');
     late final JobDetailModel jobDetailModel;
     if(id == 1){
       jobDetailModel =  JobDetailModel(
@@ -82,6 +121,74 @@ class FakeDioClient implements DioClient {
         userId: 2,
         userName: '김경제',
         content: '팀원 구합니다',
+        imageUrl: [
+          'https://picsum.photos/200/300',
+          'https://picsum.photos/200/300',
+          'https://picsum.photos/200/300',
+        ],
+      );
+    }else if(id ==3){
+      jobDetailModel =  JobDetailModel(
+        id: 3,
+        userMajor: '통계학부',
+        userStudentNumber: 19,
+        title: '종프 팀원 구합니다',
+        date: DateTime(2021, 10, 10),
+        thumbnailUrl: 'https://picsum.photos/200/300',
+        userId: 3,
+        userName: '김통계',
+        content: '종프 팀원 구합니다',
+        imageUrl: [
+          'https://picsum.photos/200/300',
+          'https://picsum.photos/200/300',
+          'https://picsum.photos/200/300',
+        ],
+      );
+    }else if(id ==4){
+      jobDetailModel =  JobDetailModel(
+        id: 4,
+        userMajor: '경제학과',
+        userStudentNumber: 22,
+        title: 'ㅇㅇㅇ 구합니다',
+        date: DateTime(2023, 1, 10),
+        thumbnailUrl: 'https://picsum.photos/200/300',
+        userId: 4,
+        userName: '김경제',
+        content: 'ㅇㅇㅇ 구합니다',
+        imageUrl: [
+          'https://picsum.photos/200/300',
+          'https://picsum.photos/200/300',
+          'https://picsum.photos/200/300',
+        ],
+      );
+    }else if(id ==5){
+      jobDetailModel =  JobDetailModel(
+        id: 5,
+        userMajor: '컴퓨터학부',
+        userStudentNumber: 19,
+        title: 'ㅁㄹㄹㄹ 팀원 구합니다',
+        date: DateTime(2021, 10, 10),
+        thumbnailUrl: 'https://picsum.photos/200/300',
+        userId: 5,
+        userName: '김컴퓨터',
+        content: 'ㅁㄹㄹㄹ 팀원 구합니다',
+        imageUrl: [
+          'https://picsum.photos/200/300',
+          'https://picsum.photos/200/300',
+          'https://picsum.photos/200/300',
+        ],
+      );
+    }else if(id ==6){
+      jobDetailModel =  JobDetailModel(
+        id: 6,
+        userMajor: '경제학과',
+        userStudentNumber: 22,
+        title: 'ㅇㅇㅇㅇ팀원 구합니다',
+        date: DateTime(2023, 1, 10),
+        thumbnailUrl: 'https://picsum.photos/200/300',
+        userId: 6,
+        userName: '김경제',
+        content: 'ㅇㅇㅇㅇ팀원 구합니다',
         imageUrl: [
           'https://picsum.photos/200/300',
           'https://picsum.photos/200/300',
