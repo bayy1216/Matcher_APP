92814a27c56c3ba7de1c0b3999efaa508d46a043
Hong <bayyyy@naver.com>
2023-10-11 22:04:29 +0900

    Feat: 이메일 로그인 화면 구현

--- lib/login/view/native_login_screen.dart
+++ lib/login/view/native_login_screen.dart
@@ -1,11 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../common/const/data.dart';
+import '../../common/const/text_style.dart';
+import '../../common/layout/default_layout.dart';
+import '../../reservation/view/reservation_screen.dart';
+import '../component/login_text_form_field.dart';
 
 class NativeLoginScreen extends StatelessWidget {
   static String get routeName => 'native_login';
+
   const NativeLoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+    final TextEditingController emailController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+    return DefaultLayout(
+      title: '',
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: DEFAULT_PADDING_H),
+        child: Form(
+          key: formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text('이메일 로그인', style: TITLE_LARGE1_STYLE),
+              const SizedBox(height: 30),
+              LoginTextFormField(
+                hintText: '이메일을 입력해주세요',
+                keyboardType: TextInputType.emailAddress,
+                controller: emailController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return '이메일을 입력해주세요';
+                  } else if (!RegExp(
+                    r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
+                  ).hasMatch(value)) {
+                    return '올바른 이메일 형식을 입력해주세요.';
+                  }
+                  return null;
+                },
+                textInputAction: TextInputAction.next,
+              ),
+              const SizedBox(height: 10),
+              LoginTextFormField(
+                hintText: '비밀번호를 입력해주세요',
+                controller: passwordController,
+                textInputAction: TextInputAction.done,
+                isObscure: true,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return '비밀번호를 입력해주세요';
+                  }
+                  return null;
+                },
+                onFieldSubmitted: (_) {
+                  if (formKey.currentState!.validate()) {
+                    context.goNamed(ReservationScreen.routeName);
+                  }
+                },
+              ),
+              const SizedBox(height: 10),
+              TextButton(
+                onPressed: () async {
+                  if (formKey.currentState!.validate()) {
+                    context
+                        .goNamed(ReservationScreen.routeName); //로그인 함수로 나중에 변경
+                    //ref.read(userProvider.notifier).login();
+                  }
+                },
+                child: Text(
+                  '로그인',
+                  style: CONTENT_MEDIUMN2_STYLE.copyWith(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+
+
+
 }
