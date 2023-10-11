642054d1dd741bebde045b291575c8d77107bbfc
Hong <bayyyy@naver.com>
2023-10-11 22:31:50 +0900

    Feat: 회원가입 화면 구현

--- lib/login/view/signup_screen.dart
+++ lib/login/view/signup_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../common/const/data.dart';
+import '../../common/const/text_style.dart';
 import '../../common/layout/default_layout.dart';
 import '../../reservation/view/reservation_screen.dart';
+import '../component/login_text_form_field.dart';
 
-class SignupScreen extends StatelessWidget {
+class SignupScreen extends ConsumerStatefulWidget {
   static String get routeName => 'signup';
   final bool isNativeSignup;
 
@@ -15,32 +18,103 @@ class SignupScreen extends StatelessWidget {
   });
 
   @override
+  ConsumerState<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends ConsumerState<SignupScreen> {
+  bool isInfoAgree = false;
+  @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
     return DefaultLayout(
       title: '',
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: DEFAULT_PADDING_H),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text('회원정보를\n입력해 주세요'),
-            TextButton(
-              onPressed: () async{
-                try {
-                  //OAuthToken token = await UserApi.instance.loginWithKakaoTalk();
-                  //print('카카오톡으로 로그인 성공 ${token.accessToken}');
-                } catch (error) {
-                  print('카카오톡으로 로그인 실패 $error');
-                }
-                context.goNamed(ReservationScreen.routeName);//로그인 함수로 나중에 변경
-              },
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.blue,
+        child: Form(
+          key: formKey,
+          child: ListView(
+            children: [
+              const Text('회원정보를\n입력해 주세요', style: TITLE_LARGE1_STYLE),
+              const SizedBox(height: 30),
+              if (widget.isNativeSignup)
+                _NativeSignupInput(),
+              Text('학번', style: CONTENT_SMALL_STYLE.copyWith(color: Colors.black),),
+              const SizedBox(height: 4),
+              LoginTextFormField(
+                hintText: '학번 2자리를 입력해주세요',
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return '학번을 입력해주세요';
+                  }
+                  else if(value.length != 2){
+                    return '학번 2자리를 입력해주세요';
+                  }
+                  return null;
+                },
+                textInputAction: TextInputAction.next,
               ),
-              child: Text('회원가입'),
-            ),
-            SignUpTextFormField(),
-          ],
+              const SizedBox(height: 10),
+              Text('학과', style: CONTENT_SMALL_STYLE.copyWith(color: Colors.black),),
+              const SizedBox(height: 4),
+              LoginTextFormField(
+                hintText: '학과를 입력해주세요',
+                keyboardType: TextInputType.text,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return '학과를 입력해주세요';
+                  }
+                  return null;
+                },
+                textInputAction: TextInputAction.next,
+              ),
+              const SizedBox(height: 10),
+              Text('닉네임', style: CONTENT_SMALL_STYLE.copyWith(color: Colors.black),),
+              const SizedBox(height: 4),
+              LoginTextFormField(
+                hintText: '닉네임을 입력해주세요',
+                keyboardType: TextInputType.text,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return '닉네임을 입력해주세요';
+                  }
+                  return null;
+                },
+                textInputAction: TextInputAction.next,
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  Checkbox(
+                    value: isInfoAgree,
+                    onChanged: (value) {
+                      setState(() {
+                        isInfoAgree = value!;
+                      });
+                    },
+                  ),
+                  Text('개인정보 처리방침에 동의합니다.',style: CONTENT_SMALL_STYLE.copyWith(color: Colors.black),),
+                ],
+              ),
+              TextButton(
+                onPressed: () {
+                  if(formKey.currentState!.validate()){
+                    if(!isInfoAgree){
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('개인정보 처리방침에 동의해주세요.'),
+                          duration: const Duration(seconds: 1),
+                        ),
+                      );
+                      return;
+                    }
+                    context.goNamed(ReservationScreen.routeName);//로그인 함수로 나중에 변경
+                  }
+                },
+                child: Text('회원가입', style: CONTENT_MEDIUMN2_STYLE.copyWith(color: Colors.white),),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -48,13 +122,48 @@ class SignupScreen extends StatelessWidget {
 }
 
 
-class SignUpTextFormField extends StatelessWidget {
-  const SignUpTextFormField({super.key});
+
+class _NativeSignupInput extends StatelessWidget {
+  const _NativeSignupInput({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text('이메일', style: CONTENT_SMALL_STYLE.copyWith(color: Colors.black),),
+        const SizedBox(height: 4),
+        LoginTextFormField(
+          hintText: '이메일을 입력해주세요',
+          keyboardType: TextInputType.emailAddress,
+          validator: (value) {
+            if (value!.isEmpty) {
+              return '이메일을 입력해주세요';
+            } else if (!RegExp(
+              r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
+            ).hasMatch(value)) {
+              return '올바른 이메일 형식을 입력해주세요.';
+            }
+            return null;
+          },
+          textInputAction: TextInputAction.next,
+        ),
+        const SizedBox(height: 10),
+        Text('비밀번호', style: CONTENT_SMALL_STYLE.copyWith(color: Colors.black),),
+        const SizedBox(height: 4),
+        LoginTextFormField(
+          hintText: '비밀번호를 입력해주세요',
 
+          textInputAction: TextInputAction.done,
+          validator: (value) {
+            if (value!.isEmpty) {
+              return '비밀번호를 입력해주세요';
+            }
+            return null;
+          },
+        ),
+        const SizedBox(height: 10),
+      ],
     );
   }
 }
