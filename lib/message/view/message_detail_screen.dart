aa38d3ac571aed59cb09b0fc21ca7884e78c20ab
Hong <bayyyy@naver.com>
2023-10-21 10:55:04 +0900

    Feat: 메세지 전송 기능 틀 추가

--- lib/message/view/message_detail_screen.dart
+++ lib/message/view/message_detail_screen.dart
@@ -1,7 +1,11 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../common/component/search_text_form_field.dart';
 import '../../common/const/color.dart';
+import '../../common/const/data.dart';
 import '../../common/layout/default_layout.dart';
 import '../component/message_detail_item.dart';
 import '../model/message_model.dart';
@@ -24,6 +28,8 @@ class MessageDetailScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final textEditingController = TextEditingController();
+    final focusNode = FocusNode();
     final state = ref.watch(messageDetailProvider(id));
     final List<MessageModel> messageList = state.when(
       data: (data) => data,
@@ -32,18 +38,55 @@ class MessageDetailScreen extends ConsumerWidget {
     );
     return DefaultLayout(
       title: '메시지 목록',
-      body: ListView.separated(
-        itemCount: messageList.length,
-        itemBuilder: (context, index) {
-          return MessageDetailItem.fromModel(model: messageList[index]);
-        },
-        separatorBuilder: (context, index){
-          return const Divider(
-            color: DIVIDIER_GREY,
-            thickness: 0.5,
-          );
-        },
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.separated(
+              itemCount: messageList.length,
+              itemBuilder: (context, index) {
+                return MessageDetailItem.fromModel(model: messageList[index]);
+              },
+              separatorBuilder: (context, index) {
+                return const Divider(
+                  color: DIVIDIER_GREY,
+                  thickness: 0.5,
+                );
+              },
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: DEFAULT_WIDGET_PADDING, vertical: 8.0),
+            height: 50,
+            child: Row(
+              children: [
+                Expanded(
+                  child: SearchTextFormField(
+                    controller: textEditingController,
+                    focusNode: focusNode,
+                    hintText: '메시지를 입력하세요',
+                    onSubmitted: (message) {
+                      _sendMessage(textEditingController);
+                    },
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    _sendMessage(textEditingController);
+                    focusNode.unfocus();
+                  },
+                  icon: const Icon(Icons.send),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  Future<void> _sendMessage(TextEditingController controller) async{
+    log('message: ${controller.text}');
+    controller.text = '';
+    //TODO: SEND MESSAGE
   }
 }
